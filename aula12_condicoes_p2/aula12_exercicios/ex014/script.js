3918aee60e6b022fb1d9078760d5810da556cd17
Eliel Laynes <ElielLaynes@users.noreply.github.com>
2021-01-29 19:25:52 -0300

    exercicios

--- aula12_condicoes_p2/aula12_exercicios/ex014/script.js
+++ aula12_condicoes_p2/aula12_exercicios/ex014/script.js
@@ -1,1 +1,17 @@
-alert('ola')
+
+function carregar() {
+var msg = document.getElementById('msg')
+var imag = document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML = `Agora são ${hora} horas.`
+if (hora >= 0 && hora < 12) {
+    // Bom dia
+    imag.src = 'manha.png'
+} else if (hora >= 12 && hora < 18) {
+    // Boa tarde
+    imag.src = 'tarde.png'
+} else {
+    imag.src = 'noite.png'
+}
+}
