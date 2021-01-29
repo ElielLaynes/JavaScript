7bcc7c9da1d3930c6d7d7881234876c41b5a02b0
Eliel Laynes <ElielLaynes@users.noreply.github.com>
2021-01-29 19:37:04 -0300

    ajustes de leiaute

--- aula12_condicoes_p2/aula12_exercicios/ex014/script.js
+++ aula12_condicoes_p2/aula12_exercicios/ex014/script.js
@@ -3,15 +3,19 @@ function carregar() {
 var msg = document.getElementById('msg')
 var imag = document.getElementById('imagem')
 var data = new Date()
-var hora = data.getHours()
+//var hora = data.getHours()
+var hora = 8
 msg.innerHTML = `Agora são ${hora} horas.`
 if (hora >= 0 && hora < 12) {
     // Bom dia
     imag.src = 'manha.png'
+    document.body.style.background = '#445b48'
 } else if (hora >= 12 && hora < 18) {
     // Boa tarde
     imag.src = 'tarde.png'
+    document.body.style.background = '#c37728'
 } else {
     imag.src = 'noite.png'
+    document.body.style.background = '#063641'
 }
 }
