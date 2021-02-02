c5f5f030673c3a3207be0eb2c2d274bb7bdb946e
Eliel Laynes <ElielLaynes@users.noreply.github.com>
2021-02-02 19:25:45 -0300

    Create ambiente.js

---
+++ aulas13_e_14/ambiente.js
@@ -0,0 +1,45 @@
+// Estutura de Repetição com Teste Lógico no INÍCIO
+
+// Primeiro faz o teste lógico e depois executa o bloco.
+/*var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ // contado recebe ele + 1
+}
+
+// Estutura de Repetição com Teste Lógico no FINAL (do - while)
+
+// Executa o bloco e depois faz o teste lógico
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6) */
+
+//Observação: While é recomendado para quando eu não sei o limite que preciso até para a execução.
+
+
+
+// Estutura de Repetição Com Variável de Controle (ideial caso a gente saiba o limites que queremos de execução)
+/*
+ for (inicio; teste lógico; incremento) {
+    bloco que faz o incremento
+} 
+    Exemplo:
+
+        for (c = 1; c <= 6; c++) {
+
+        }
+*/
+
+for (c = 1; c <= 5; c++) {
+    console.log(c)
+}
+
+
+
+
+
+
+
+
