1d09f983394bae1b9ed5636fe5602eff4959208e
Eliel Laynes <ElielLaynes@users.noreply.github.com>
2021-02-02 18:43:47 -0300

    exercicio 15

--- aula12_condicoes_p2/aula12_exercicios/ex014/script.js
+++ aula12_condicoes_p2/aula12_exercicios/ex014/script.js
@@ -3,8 +3,8 @@ function carregar() {
 var msg = document.getElementById('msg')
 var imag = document.getElementById('imagem')
 var data = new Date()
-//var hora = data.getHours()
-var hora = 8
+var hora = data.getHours()
+// var de teste -> var hora = 8
 msg.innerHTML = `Agora são ${hora} horas.`
 if (hora >= 0 && hora < 12) {
     // Bom dia
